ae767b6b9e2851e3201304a2d948335ffea24c7b
powertechnaija <67103280+powertechnaija@users.noreply.github.com>
2025-03-05 11:18:14 +0100

    Add client/src/components/ui/index.js 1741169893616

---
+++ client/src/components/ui/index.js
@@ -0,0 +1,22 @@
+export { default as Alert, InfoAlert, SuccessAlert, WarningAlert, ErrorAlert } from './Alert';
+export { default as Badge, StatusBadge, OrderStatusBadge } from './Badge';
+export { default as Breadcrumbs } from './Breadcrumbs';
+export { default as Button } from './Button';
+export { default as Card, CardHeader, CardTitle, CardSubtitle, CardBody, CardFooter, ProductCard } from './Card';
+export { default as Dropdown } from './Dropdown';
+export { default as ImageGallery } from './ImageGallery';
+export { default as Input } from './Input';
+export { default as LoadingOverlay } from './LoadingOverlay';
+export { default as Modal, ConfirmModal } from './Modal';
+export { default as NotificationStack } from './NotificationStack';
+export { default as Pagination } from './Pagination';
+export { default as Rating, RatingWithLabel, RatingInput } from './Rating';
+export { default as ScrollToTop } from './ScrollToTop';
+export { default as Select } from './Select';
+export { default as Table } from './Table';
+export { default as Tabs } from './Tabs';
+export { default as Textarea } from './Textarea';
+
+// Example usage:
+// import { Button, Card, Modal } from '@/components/ui';
+// This imports multiple components in a single line instead of separate imports
